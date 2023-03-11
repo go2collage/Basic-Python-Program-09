7d64db0173e2ed5e6e94e7b54e247fa486988342
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-11 22:35:15 +0530

    Add files via upload

---
+++ BP_P9.py
@@ -0,0 +1,9 @@
+# Basic Python Program
+
+# Find Absolute value
+
+num = float(input("Enter a number: "))
+
+print("given Absolute value is: ", abs(num))
+
+# by using abs() method we get absolute value of a number
